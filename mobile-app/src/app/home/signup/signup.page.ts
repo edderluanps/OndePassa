5c16b00c60ad504b5cd073abddb429024e2adb1f
edderluanps <edder.pessoa@gaiacode.com.br>
2025-02-08 17:56:02 -0300

    Pagina de Signup

--- mobile-app/src/app/home/signup/signup.page.ts
+++ mobile-app/src/app/home/signup/signup.page.ts
@@ -1,18 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent,AlertController, IonHeader, IonTitle, IonToolbar, IonButton, IonItem, IonInput, IonInputPasswordToggle} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ IonInput, IonInputPasswordToggle, IonButton, CommonModule, FormsModule]
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router : Router,
+    private alertController: AlertController
+  ) {}
+
+    async presentAlert() {
+      const alert = await this.alertController.create({
+        header: 'Seja bem vind@',
+        subHeader: 'Bem vind@',
+        message: '',
+        buttons: ['Fechar'],
+      });
+
+      await alert.present();
+    }
+  
+    goToProfile(){
+      this.router.navigate(['/perfil']);
+      this.presentAlert();
+    }
+  
+    goToHome(){
+      this.router.navigate(['/']);
+    }
 
   ngOnInit() {
   }
